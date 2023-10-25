38bd13c749c47f8f1059bc67fe531886f7289e52
Vygantas Vilius <biesaz@gmail.com>
2023-10-25 22:42:26 +0300

    push

---
+++ main.py
@@ -0,0 +1,38 @@
+# def print_argument(arg_one: str, arg_two: str) -> None
+#     print(f"got arg_one: {arg_one} arg two {arg_two}")
+# my_parametr = {"arg_one": 1, "rag_two": 2} #arg_one =1, arg_two = 2
+# print_argument(**my_parametr)
+
+# def sum_all_numbers(*args: int) -> int:
+#     return sum([num for num in args])
+
+# my_numbers = [1,1,1,]
+
+# print(sum_all_numbers(1,2,3))
+war = [1, 2 ,3]
+def sum_list(numers: list) -> int:
+    return sum(numers)
+
+print(sum_list(war))
+
+def sum_arguments(num_a: int, num_b: int, num_c: int) -> int:
+    return num_a + num_b + num_c
+
+def sum_args(*args: int) -> int:
+    return sum(args)
+
+print(sum_args(1, 2, 3))
+
+from typing import Union
+
+def calculator(sign: str, *args) -> Union[int, float]:
+    if sign =="+":
+        return sum(args)
+    if sign == "-":
+        return args[0] - sum(args[1:])
+    if sign == "*":
+        res = 1
+        for num in args:
+            res *= num
+        return res
+print(calculator("-", 1, 2, 3))
